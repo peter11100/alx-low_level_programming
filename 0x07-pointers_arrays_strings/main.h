9f764b0b889823e26b76c85bff0927cbcf473779
peter11100 <peterabimbola555@gmail.com>
2023-10-23 20:49:39 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <string.h>
 #include <stdio.h>
 
 int _putchar(char c);
@@ -13,4 +14,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
 #endif
